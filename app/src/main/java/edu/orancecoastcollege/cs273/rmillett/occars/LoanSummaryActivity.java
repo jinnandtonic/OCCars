ec1355eacca842b2732a79a765b3aa9df2fe204c
ryanmillett <rmillett@student.cccd.edu>
2017-09-14 15:56:48 -0700

    Initial commit

--- app/src/main/java/edu/orancecoastcollege/cs273/rmillett/occars/LoanSummaryActivity.java
+++ app/src/main/java/edu/orancecoastcollege/cs273/rmillett/occars/LoanSummaryActivity.java
@@ -3,11 +3,11 @@ package edu.orancecoastcollege.cs273.rmillett.occars;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PurchaseActivity extends AppCompatActivity {
+public class LoanSummaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_purchase_old);
+        setContentView(R.layout.activity_loan_summary);
     }
 }
