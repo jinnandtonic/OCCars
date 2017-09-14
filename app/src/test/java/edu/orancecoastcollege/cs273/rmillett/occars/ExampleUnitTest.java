4a6640606b3f1159ba4aa0410abcbcbbc00da970
ryanmillett <rmillett@student.cccd.edu>
2017-09-14 14:45:32 -0700

    Initial commit

---
+++ app/src/test/java/edu/orancecoastcollege/cs273/rmillett/occars/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.orancecoastcollege.cs273.rmillett.occars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
